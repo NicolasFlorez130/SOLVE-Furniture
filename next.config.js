27d5ec5b656306962b7d5d4640170d18f44e5e0e
NicolasFlorez130 <nmflorezr@gmail.com>
2022-10-08 15:48:22 -0500

    Making initializable from localhost again done

--- next.config.js
+++ next.config.js
@@ -1,15 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-   basePath: '/SOLVE-Furniture',
    productionBrowserSourceMaps: true,
    reactStrictMode: true,
    swcMinify: true,
    images: {
-      loader: 'akamai',
-      path: '',
       domains: ['localhost', 'fathomless-scrubland-17753.herokuapp.com'],
-   },
-   assetPrefix: '/SOLVE-Furniture',
+   }
 }
 
 const withTM = require('next-transpile-modules')(['gsap', 'smooth-scrollbar'])
